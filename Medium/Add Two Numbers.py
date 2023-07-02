ad2157543571247c814943699f8820f00e1eaf74
ALekator <60851776+alekator@users.noreply.github.com>
2023-07-02 22:23:55 +0700

    Create

---
+++ Medium/Add Two Numbers.py
@@ -0,0 +1,45 @@
+# 2. Add Two Numbers
+
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+# Example 1:
+# Input: l1 = [2,4,3], l2 = [5,6,4]
+# Output: [7,0,8]
+# Explanation: 342 + 465 = 807.
+
+# Example 2:
+# Input: l1 = [0], l2 = [0]
+# Output: [0]
+
+# Example 3:
+# Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+# Output: [8,9,9,9,0,0,0,1]
+
+# Constraints:
+# 1. The number of nodes in each linked list is in the range [1, 100].
+# 2. 0 <= Node.val <= 9
+# 3. It is guaranteed that the list represents a number that does not have leading zeros.
+
+# Answer:(Nodes is good)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        f = n = ListNode(0)
+        num1 = num2 = ""
+        while l1:
+            num1 += str(l1.val)
+            l1 = l1.next
+        while l2:
+            num2 += str(l2.val)
+            l2 = l2.next
+        res = str(int(num1[::-1]) + int(num2 [::-1]))[::-1]
+        for i in res:
+            f.next = ListNode (i)
+            f = f.next
+        return n.next
+
